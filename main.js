aa9baf02e69de97a7dadc1bb1fa0fab990efe8b3
kamen <kamkanev@gmail.com>
2020-02-09 11:32:50 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+document.body.onload = addElement;
+
+function addElement () {
+  // create a new div element
+  var newDiv = document.createElement("div");
+  // and give it some content
+  var newContent = document.createTextNode("Hi there and greetings!");
+
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+newDiv.style.color = color;
+
+  // add the newly created element and its content into the DOM
+  var currentDiv = document.getElementById("div1");
+  document.body.insertBefore(newDiv, currentDiv);
+}
